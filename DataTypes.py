48e849db5e6351f54c8736b7ea60cc043dc00b48
briavery <157061193+briavery@users.noreply.github.com>
2024-01-25 11:02:10 -0600

    Add files via upload

---
+++ DataTypes.py
@@ -0,0 +1,21 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = 20
+print("Literal:", x)
+
+# Integer
+y = -30
+print("Integer:", y)
+
+# Float
+z = 45.7
+print("Float:", z)
+
+# String
+name = "Briana"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
