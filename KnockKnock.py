d123febb49382364aad832da33a36d80cce766c0
briavery <157061193+briavery@users.noreply.github.com>
2024-01-29 17:43:31 -0600

    Add files via upload

---
+++ KnockKnock.py
@@ -0,0 +1,9 @@
+# Knock-Knock Jokes Using Multiple Print Commands
+print("Knock, knock.")  
+print("Who's there?")  
+print("Dwayne.")  
+print("Dwayne who?")  
+print("Dwayne the tub I'm dwowning!")  
+
+# Knock-Knock Jokes All on One Line of Command
+print("Knock, knock.\nWho's there?\nDwayne.\nDwayne who?\nDwayne the tub i'm dwowning!")
