85daf27acd5b6689a4a2290a3246840ac2d6dd58
briavery <157061193+briavery@users.noreply.github.com>
2024-02-05 21:59:21 -0600

    Add files via upload

---
+++ PythonComments.py
@@ -0,0 +1,8 @@
+#declares value for variable tank_size
+tank_size = 15
+#stores 600 in variable distance
+distance = 600
+#calculates mpg using variables above
+mpg = distance / tank_size
+#Prints mpg based on calculations
+print("Miles per gallon:", mpg)
