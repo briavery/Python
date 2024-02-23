55e0420dc3cb443a680259ae4dd99c6abd2f8f78
briavery <157061193+briavery@users.noreply.github.com>
2024-02-23 11:57:01 -0600

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,10 @@
+def square(side):
+    area = side * side
+    print(f"The area of the square is {area} square units.")
+
+def circle(radius):
+    area = 3.14 * radius * radius
+    print(f"The area of the circle is {area} square units.")
+
+square(4)
+circle(5)
