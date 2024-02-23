323ca56ef50c922b8a0eeb24c1a5b3e92995800c
briavery <157061193+briavery@users.noreply.github.com>
2024-02-23 13:17:51 -0600

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,22 @@
+def custom_song(verb, verb2, animal, place, noun, noun2, adjective, adjective2):
+    print("\n\n")
+    print(f"{verb} {verb} little {animal}, ")
+    print(f"how I {verb2} what you are. ")
+    print(f"Up above the {place} so high, ")
+    print(f"like a {noun} in the {noun2}. ")
+    print(f"{verb} {verb} little {animal}, ")
+    print(f"how I {verb2} what you are.")
+    print("\n")
+    print(f"When the {adjective} sun is set, ")
+    print(f"and the grass with dew is {adjective2}")
+
+verb = input("Enter a verb: ")
+verb2 = input("Enter another verb: ")
+animal = input("Enter an animal: ")
+place = input("Enter a place: ")
+noun = input("Enter a noun: ")
+noun2 = input("Enter another noun: ")
+adjective = input("Enter an adjective: ")
+adjective2 = input ("Enter another adjective: ")
+
+custom_song(verb, verb2, animal, place, noun, noun2, adjective, adjective2)
